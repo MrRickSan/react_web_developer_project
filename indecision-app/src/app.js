b7e901b8bf934bf8cc6a34712dee1f275ea5d9df
Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-09-23 00:20:23 +0100

    binding

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -39,8 +39,12 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+    constructor (props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
     handleRemoveAll () {
-        alert('handleRemoveAll')
+        alert(this.props.options);
     }
 
     render () {
@@ -69,7 +73,7 @@ class AddOption extends React.Component {
     handleAddOption (e) {
         e.preventDefault();
 
-        const option = e.target.elements.option.value;
+        const option = e.target.elements.option.value.trim();
 
         if (option) {
             alert(option);
