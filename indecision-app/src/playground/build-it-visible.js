90eed4cbb867b5bee7ef4f0a4c4a61b5f3c0efd8
Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-09-25 00:01:51 +0100

    Refactoring build-it-visible.js to use react components

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,26 +1,37 @@
-let visibility = false;
+class VisibilityToggle extends React.Component{
+    constructor (props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility: false
+        }
+    }
 
-const toggleVisibility = () => {
-    visibility = !visibility;
-    render();
-};
+    handleToggleVisibility () {
+        this.setState ( (prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        });
+    }
 
-const render = () => {
-    const jsx = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility} >
-                {visibility ? 'Hide Details' : 'Show Details'}
-            </button>
-            {visibility && (
-                <div>
-                    <p>Hey. These are some details you can now see!</p>
-                </div>
-            )}
-        </div>
-    );
+    render () {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleToggleVisibility}>
+                    {this.state.visibility ? 'Hide Details' : 'Show Details'}
+                </button>
+                {
+                    this.state.visibility && (
+                        <div>
+                            <p>Hey. These are some details you can now see!</p>
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
 
-    ReactDOM.render(jsx, document.getElementById('app'))
-};
-
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
