32190188a54c7bac0fe69efdb44dcf3a8c7409a3
Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-10-02 00:39:46 +0100

    webpack running babel and compnents in sparated files

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,5 @@
-import subtract, { square, add } from './utils';
-import isSenior, { isAdult, canDrink } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-console.log('app.js is running!!!');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(isAdult(18));
-console.log(canDrink(17));
-console.log(subtract(100, 23));
-console.log(isSenior(64));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- indecision-app/src/person.js
+++
@@ -1,9 +0,0 @@
-console.log('person.js is runnig!');
-
-const isAdult = (age) => age >= 18;
-
-const canDrink = (age) => age >= 18;
-
-const isSenior = (age) => age >= 65;
-
-export { isAdult, canDrink, isSenior as default }
